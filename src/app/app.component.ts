7389258b79efacc721081d6d2667af8eaf187e94
Peter <sikk1996@gmail.com>
2020-10-01 19:57:26 +0200

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyCrew';
+  fillerNav = [
+    'Home',
+    'Settings',
+    'Admin',
+  ];
+
+
+
 }
